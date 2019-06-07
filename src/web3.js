5aee45f05e63c157bed667e45b4d97936523e54a
Steeve Payraudeau <steeve.payraudeau@epitech.eu>
2019-06-07 15:04:51 +0400

    add test truffle

--- src/web3.js
+++ src/web3.js
@@ -1,11 +1,21 @@
 import Web3 from "web3";
 
-// if (typeof window.web3 !== 'undefined') {
-const getProvider = async () => {
-  await window.web3.currentProvider.enable(); // request authentication
-};
-getProvider();
+let web3;
 
-const web3 = new Web3(window.web3.currentProvider);
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // in the browser and the user has injected Metamask
+  const getProvider = async () => {
+    await window.web3.currentProvider.enable(); // request authentication
+  };
+  getProvider();
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // we are on the server OR the user is not running metamask
+  const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/aa7a461490244b65a54e2f67b959fa8a');
+  web3 = new Web3(provider);
+}
+
+
+
 
 export default web3;
